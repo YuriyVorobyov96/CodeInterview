308cb8384013f6bd2d36b3123b5e6fbdd88e5816
Yuriy Vorobyov <darthrevan1333@gmail.com>
2022-05-25 08:58:50 +0000

    добавлена задача по типизации через enum

---
+++ enum-to-interface.ts
@@ -0,0 +1,24 @@
+/*
+  Имеется enum Colors, необходимо создать протипизировать объект obj
+  таким образом, чтобы при добавлении ключа, отсутствующего в enum, выдавалась   ошибка
+
+  Программист уже написал интерфейс, но его работа некорректна
+*/
+
+export enum Colors {
+  RED = 'red',
+  GREEN = 'green',
+  BLUE = 'blue',
+}
+
+interface ColorInterface {
+  [key: string]: boolean;
+}
+
+const obj: ColorInterface = {
+  green: true,
+  blue: true,
+  test: true, // нет ошибки
+};
+
+console.log(obj);
