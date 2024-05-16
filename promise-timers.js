6bf29f4078eff644770bc5044a8068098c4fdd3f
YuriyVorobyov <>
2024-05-16 08:20:55 +0000

    добавлена классическая задачи на промисы

---
+++ promise-timers.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+  console.log('timeout')
+}, 0);
+
+const p = new Promise((resolve, reject) => {
+  console.log('Promise creation');
+  resolve()
+})
+
+const p2 = new Promise((resolve, reject) => {
+  console.log(123)
+})
+
+p.then(() => {
+  console.log('Promise resolving');
+})
+
+console.log('End')
+
+console.log('p2 =>>', p2)
