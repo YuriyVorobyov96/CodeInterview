baa00e8288c7dad3254aba680918d6094e07129d
YuriyVorobyov <>
2024-12-05 08:39:05 +0000

    update tasks

--- sql_users-departments-salaries.sql
+++ sql_users-departments-salaries.sql
@@ -4,38 +4,40 @@
 -- Получить среднюю зарплату каждого отдела, но включать только те отделы, где средняя зарплата превышает 50 000 долларов США.
 
 -- DEPARTMENTS TABLE
-CREATE TABLE departments
+CREATE OR REPLACE TABLE departments
 (
-  id INTEGER PRIMARY KEY,
+  id INTEGER NOT NULL PRIMARY KEY,
   name VARCHAR(255)
-);
-
--- USERS TABLE
-CREATE TABLE users
-(
-  id INTEGER PRIMARY KEY,
-  name_full VARCHAR(255),
-  department_id INTEGER REFERENCES departments(id),
-  salary_id INTEGER REFERENCES salaries(id)
-);
+) ENGINE=InnoDB;
 
 -- SALARIES TABLE
-CREATE TABLE salaries
+CREATE OR REPLACE TABLE salaries
 (
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
+  id INTEGER NOT NULL PRIMARY KEY,
+  user_id INTEGER,
   salary INTEGER
-);
+) ENGINE=InnoDB;
+
+-- USERS TABLE
+CREATE OR REPLACE TABLE users
+(
+  id INTEGER NOT NULL PRIMARY KEY,
+  name_full VARCHAR(255),
+  department_id INTEGER NOT NULL,
+  salary_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments (id),
+  FOREIGN KEY (salary_id) REFERENCES salaries (id)
+) ENGINE=InnoDB;
 
 -- Inserting values into DEPARTMENTS table
-INSERT INTO departments (id, name) VALUES
+INSERT IGNORE INTO departments (id, name) VALUES
 (1, 'HR'),
 (2, 'Finance'),
 (3, 'Marketing'),
 (4, 'Engineering');
 
 -- Inserting values into USERS table
-INSERT INTO users (id, name_full, department_id, salary_id) VALUES
+INSERT IGNORE INTO users (id, name_full, department_id, salary_id) VALUES
 (1, 'John Doe', 1, 1),
 (2, 'Jane Smith', 2, 2),
 (3, 'Michael Johnson', 3, 3),
@@ -46,7 +48,7 @@ INSERT INTO users (id, name_full, department_id, salary_id) VALUES
 (8, 'Jessica Taylor', 1, 8);
 
 -- Inserting values into SALARIES table
-INSERT INTO salaries (id, user_id, salary) VALUES
+INSERT IGNORE INTO salaries (id, user_id, salary) VALUES
 (1, 1, 50000),
 (2, 2, 60000),
 (3, 3, 70000),
@@ -55,3 +57,4 @@ INSERT INTO salaries (id, user_id, salary) VALUES
 (6, 6, 65000),
 (7, 7, 62000),
 (8, 8, 58000);
+
